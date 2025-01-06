529675dcb27a3190e5fc7db3bbf3ddbc5360b034
Angelos Tsiaras <atsiaras@star.ucl.ac.uk>
2025-01-06 10:40:50 +0200

    Update __init__.py

--- hops/__init__.py
+++ hops/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = '3.1.2'
+__version__ = '3.1.3'
 __message__ = 'Version 3.1 is now online with many new features!\n3.1.1 - Many bugs fixed, improved alignment, option to load location from profile, option to crop image edges.\n3.1.2 - Version control for twirl'
 
 from .__run__ import run_app
